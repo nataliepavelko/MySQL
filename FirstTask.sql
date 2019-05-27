f3fa7459975e72b07793b94b2d13afe784974ec9
Natalie <33936649+nataliepavelko@users.noreply.github.com>
2019-05-27 22:01:06 +0300

    Update FirstTask.sql

--- FirstTask.sql
+++ FirstTask.sql
@@ -6,7 +6,7 @@ create table developers
 (id int auto_increment primary key,
 name varchar (50) NOT NULL,
 surname varchar (50) NOT NULL,
-sex ENUM ("F", "M"));
+sex VARCHAR (10)); -- 
 
 create table skills (
 id int auto_increment primary key,
@@ -15,15 +15,19 @@ level varchar (20) NOT NULL);
 
 create table projects 
 (id int auto_increment primary key,
-name varchar (50) NOT NULL);
+name varchar (50) NOT NULL,
+cost double,
+date VARCHAR(300));
 
 create table companies 
 (id int auto_increment primary key,
-name varchar (50) NOT NULL);
+name varchar (300) NOT NULL,
+address LONGTEXT); -- 
 
 create table customers 
 (id int auto_increment primary key,
-name varchar (30) NOT NULL);
+name varchar (30) NOT NULL,
+phone VARCHAR (25));
 
 create table developers_projects 
 (id_developer int,
@@ -66,29 +70,30 @@ INSERT INTO skills(name, level) VALUES
 ('Java', 'Middle'),
 ('Java', 'Senior');
 
-INSERT INTO projects (name) VALUES
-('DNA'), 
-('Corgi'),
-('Step by Step'),
-('JavaRush'),
-('Milly'),
-('Soup'),
-('NoteBook');
+INSERT INTO projects (name, cost, date) VALUES
+('DNA', 122000 , '12.05.18'), 
+('Corgi', 77500, '14.12.18'),
+('Step by Step', 95500, '04.03.19'),
+('JavaRush', 115000, '01.03.18'),
+('Milly', 80000, '07.02.19'),
+('Soup', 78000, '15.01.19'),
+('NoteBook', 65500, '08.05.19');
 
-INSERT INTO companies (name) VALUES
-('Luxoft'),
-('Cognians'),
-('Ciklum'),
-('Game Dev');
+
+INSERT INTO companies (name , address) VALUES
+('Luxoft', 'Urliska street, 20'),
+('Cognians', 'Naumova street, 37A'),
+('Ciklum', 'Pravdy Ave, 2'),
+('Game Dev', 'Lesi Ukrainky Blvd, 15');
 
 select *  from companies;
 select *  from projects;
-
-INSERT INTO customers (name) VALUES
-("MID"),
-("Axent"),
-("SortSoft"),
-("Force");
+             
+INSERT INTO customers (name , phone) VALUES
+('MID', '063 822 4118'),
+('Axent', '044 241 4141'),
+('SortSoft', '073 520 9605'),
+('Force', '050 135 6886');
 
 insert into developers_projects (id_developer, id_project) values
 (1, 1), 		    -- Наташа работает на DNA--
